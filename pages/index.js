c552fac24270d3334f4f8429c086eda0447d5a26
Ollie Thomas <ollie.thomas1992@gmail.com>
2018-10-28 09:39:45 +0000

    changed date value

--- pages/index.js
+++ pages/index.js
@@ -13,8 +13,8 @@ class Index extends Component {
 
 		// Merge and and sort by date to create feed
 		const feed = [...articles, ...matchReports].sort((a, b) => {
-			a = new Date(a.date);
-			b = new Date(b.date);
+			a = new Date(a.acf.date);
+			b = new Date(b.acf.date);
 			return a > b ? -1 : a < b ? 1 : 0;
 		})
 		// Filter out "Unpublished" Posts
